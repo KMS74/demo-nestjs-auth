ac069c1964aaaeae7ed8d2a60ad093adc817e3bf
Karem Shabana <km425360@gmail.com>
2024-11-16 19:57:27 +0200

    feat: Integrate @nestjs/config for environment variable management and refactor JWT secret handling

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,15 +3,17 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { JWT_SECRET } from 'src/configs/jwt-secret';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
-    JwtModule.register({
-      global: true, // Make the module available globally in the app
-      secret: JWT_SECRET,
-      signOptions: { expiresIn: '1d' },
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
     }),
   ],
   providers: [AuthService],
--- src/configs/jwt-secret.ts
+++
@@ -1,3 +0,0 @@
-// should be stored in secure way like .env file
-export const JWT_SECRET =
-  '7d1ff9d015922fe492b63137d7217b860d9c593aae59b8fa87129243f649e3b0';
